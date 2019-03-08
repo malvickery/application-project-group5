5a2096fe1465e6f30a907b53d49deef8a552a303
allison <allisonmspeck@gmail.com>
2019-03-08 16:12:49 -0500

    DB Migrations
    Finished creating DB migrations, still need to insert scripts into all files but migrations are there.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,21 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        DB::statement("
+CREATE TABLE `users` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  CONSTRAINT `Users_PK` PRIMARY KEY (`id`),
+  CONSTRAINT `Users_Email_UK` UNIQUE (`email`)
+
+);
+ ");
     }
 
     /**
