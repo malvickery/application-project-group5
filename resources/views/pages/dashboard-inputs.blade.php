a02dfbb5eb27a75146bafe84c5a4252b57b63d93
allison <allisonmspeck@gmail.com>
2019-04-16 15:16:56 -0400

    created table and update/save functionality
    new table called announcements and working on it sending to announcements div

---
+++ resources/views/pages/dashboard-inputs.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="col-12">
+            <div class="row">
+                <div class="card col">
+                    <div class="card-body">
+                        <h1>
+                            Update Quote
+                        </h1>
+                        <textarea></textarea>
+                    </div>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
